520e618c99a1483a0c5ebb1709d0ef1e71d837cd
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-03-13 00:09:44 +0700

    feat | init source with react, webpack and loaders

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./app";
+
+// compile App component in `#app` HTML element
+ReactDOM.render(<App />, document.getElementById("app"));
